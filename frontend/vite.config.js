49ecbaefc3330a5975afe57febe56c47118270d9
Bernd Adamczyk <bernd_adamczyk@hotmail.de>
2025-12-26 13:39:48 +0100

    vite config allowed host -> all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: process.env.VITE_ALLOWED_HOSTS
-      ? process.env.VITE_ALLOWED_HOSTS.split(',')
-      : []
+    allowedHosts: 'all'
   }
 })
 
